e94c44ae467fbf7b33a501cd2a2070b959317e20
Kirill <ekirillo@rambler.ru>
2023-10-21 02:05:38 +0500

    num13

---
+++ homework/num13/Program.cs
@@ -0,0 +1,26 @@
+﻿int ShowThirdDigit(int num)
+{
+    if(num < 100)
+    {
+        System.Console.WriteLine("третьей цифры нет");
+    }
+    if(num < 999 && num > 99)
+    {
+        num = num % 10;
+    }
+    if(num > 999)
+    {
+        while(num > 999)
+        {
+            num = num / 10;
+        }
+         num = num % 10; 
+    }
+    return num;
+}
+
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(ShowThirdDigit(num));
+
