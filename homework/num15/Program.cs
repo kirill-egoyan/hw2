49a89b58048f204344b1f8eee54f3af4474b24d8
Kirill <ekirillo@rambler.ru>
2023-10-21 02:07:48 +0500

    num15

---
+++ homework/num15/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day < 6)
+{
+    System.Console.Write("нет");
+}
+if(day > 5 && day < 8)
+{
+    System.Console.Write("да");
+}
+if(day > 7)
+{
+    System.Console.WriteLine("Не является днем недели");
+}
+
