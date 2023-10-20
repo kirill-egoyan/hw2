e32afc43a2245026416406de78a2a326499fc2ef
Kirill <ekirillo@rambler.ru>
2023-10-21 02:03:09 +0500

    num10

--- homework/num10/Program.cs
+++ homework/num10/Program.cs
@@ -1,1 +1,10 @@
-﻿https://github.com/kirill-egoyan/hw2.git
+﻿int ShowSecondDigit(int num)
+{
+    int des = num /10 % 10;
+    
+    return des;
+}
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(ShowSecondDigit(num));
+
